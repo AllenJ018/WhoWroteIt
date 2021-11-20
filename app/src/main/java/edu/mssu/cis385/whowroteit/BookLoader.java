9b29cef33919b860d011fa9ee24a813ba31d2a41
AllenJ018 <AllenJ018@mymail.mssu.edu>
2021-11-20 14:57:17 -0600

    Doens't run, unsure what I did wrong, tried error chedcking

---
+++ app/src/main/java/edu/mssu/cis385/whowroteit/BookLoader.java
@@ -0,0 +1,35 @@
+package edu.mssu.cis385.whowroteit;
+
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+
+public class BookLoader extends AsyncTaskLoader<String> {
+    private String mQueryString;
+
+    BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+    }
+
+    public BookLoader(@NonNull Context context) {
+        super(context);
+    }
+
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+}
